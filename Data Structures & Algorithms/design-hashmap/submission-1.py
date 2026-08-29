c565c4582b0f6a340ce01405a6505e42c6c9f4d4
Aryan Negi <134124253+aryan-negi-93@users.noreply.github.com>
2026-08-29 06:50:31 +0530

    Add: design-hashmap - submission-1

---
+++ Data Structures & Algorithms/design-hashmap/submission-1.py
@@ -0,0 +1,41 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.arr = []
+
+        
+
+    def put(self, key: int, value: int) -> None:
+        
+        
+        for elm in self.arr:
+            if elm[0] == key:
+                elm[1] = value
+                return 
+        
+        self.arr.append([key , value])
+                
+      
+
+    def get(self, key: int) -> int:
+        for i in range(len(self.arr)):
+            if self.arr[i][0] == key:
+                return self.arr[i][1]
+        return -1
+        
+
+    def remove(self, key: int) -> None:
+        for i in range(len(self.arr)):
+            if self.arr[i][0] == key:
+                self.arr.pop(i)
+                return
+        
+
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
