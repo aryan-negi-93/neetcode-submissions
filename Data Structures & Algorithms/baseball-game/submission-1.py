0c4665aa8cc21c0b553ed372667d8b5f8ee345df
Aryan Negi <134124253+aryan-negi-93@users.noreply.github.com>
2026-09-14 06:23:46 +0530

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+
+        stack = []
+
+        for operation in operations:
+            if operation not in ["C", "D", "+"]:
+                stack.append(int(operation))
+            else:
+                if operation == "+":
+                    stack.append(stack[-1] + stack[-2])
+                elif operation == "D":
+                    stack.append(stack[-1] * 2)
+                elif operation == "C":
+                    stack.pop()
+
+        res = 0
+        for elm in stack:
+            res += elm
+
+        return res
+
+
+        
